06b4867639a7b311c46f3ae9362babfe7d58dc0d
Maxime Druart <maxime.druart@yahoo.fr>
2020-03-05 10:23:18 +0100

    init

---
+++ bundler/webpack.config.js
@@ -0,0 +1,69 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin: cwbp } = require("clean-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
+
+module.exports = {
+	mode: "development",
+	devtool: "source-map",
+	devServer: {
+		contentBase: "./dist",
+		open: true,
+		port: 3000,
+		host: "10.93.180.254"
+	},
+	entry: path.resolve(__dirname, "../src/index.js"),
+	output: {
+		filename: "bundle.[hash].js",
+		path: path.resolve(__dirname, "../dist")
+	},
+	plugins: [
+		new cwbp(),
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "../src/index.html"),
+			minify: true
+		}),
+		new MiniCssExtractPlugin(),
+		new CopyWebpackPlugin([
+			{
+				from: "static"
+			}
+		])
+	],
+	module: {
+		rules: [
+			{
+				test: /\.html$/,
+				use: ["html-loader"]
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: ["babel-loader"]
+			},
+			{
+				test: /\.css$/,
+				use: [MiniCssExtractPlugin.loader, "css-loader", "stylus-loader"]
+			},
+			{
+				test: /\.(png|jpg|svg|gif)$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: { outputPath: "images/" }
+					}
+				]
+			},
+			{
+				test: /\.(ttf|otf|woff|woff2|eot)$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: { outputPath: "fonts/" }
+					}
+				]
+			}
+		]
+	}
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./style/main.css"
+import img from "./images/image.jpg"
+
+let $img = new Image()
+$img.src = img
+document.body.appendChild($img)
